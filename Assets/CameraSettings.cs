f7c5cfa66d3bbe5a7432a8eeae60c5e0bd605152
UnknownGamedev <unknowngamedev.el@gmail.com>
2024-08-18 16:11:20 +0530

    Added Camera to test scene

---
+++ Assets/CameraSettings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
