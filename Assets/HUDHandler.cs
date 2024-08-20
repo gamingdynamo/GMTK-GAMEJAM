cb07fd531d7823589d8b8b6f35d5763cd009daf5
CoCkMelon <rosgu69@inbox.ru>
2024-08-20 18:56:12 +0300

    Add credits screne. Remove what prevented build.

--- Assets/HUDHandler.cs
+++ Assets/HUDHandler.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.MemoryProfiler;
+// using UnityEditor.MemoryProfiler;
 using UnityEngine;
 
 public class HUDHandler : MonoBehaviour
